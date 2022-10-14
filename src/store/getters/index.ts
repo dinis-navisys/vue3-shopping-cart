8a266c2179acb1c85a2c8e98e9be2f34d484b673
Peter Dinis <dinis@navisys.cz>
2022-10-14 14:01:11 +0200

    fix: Fix problem with total price

--- src/store/getters/index.ts
+++ src/store/getters/index.ts
@@ -1,5 +1,5 @@
 import { State } from "../state";
-import { TotalPrice } from "../types/helpers";
+import { ICardItem } from "../types/ICard";
 
 export const getters = {
     getProducts: (state: State) => state.allProducts,
@@ -7,9 +7,9 @@ export const getters = {
     getAllItemsInCart: (state: State) => state.cart.length,
     getTotal: (state: State) => {
         let total = 0
-        state.cart.map((p: TotalPrice) => {
-            total += p.price;
+        state.cart.map((p: ICardItem) => {
+            total += p.price * p.quantity!
         })
         return total
-    }
+    },
 }
