3cd0d60389e779f86d552732441337a36ec4fae9
Peter Dinis <dinis@navisys.cz>
2022-10-17 11:10:53 +0200

    feat: Updating code

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,5 @@
-<script lang="ts">
+<script setup lang="ts">
 import Navbar from "./components/shared/Navbar.vue";
-import { defineComponent } from 'vue';
-
-export default defineComponent({
-    name: "App",
-    components: {
-        Navbar
-    }
-})
 </script>
 
 <template>
--- src/components/shared/Navbar.vue
+++ src/components/shared/Navbar.vue
@@ -24,14 +24,6 @@
   </nav>
 </template>
 
-<script lang="ts">
-import { defineComponent } from "vue";
-
-export default defineComponent({
-  name: "Navbar",
-});
-</script>
-
 <style>
 /* .active is from routes/index.ts */
 nav .active {
